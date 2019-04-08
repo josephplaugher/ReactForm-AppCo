153d3059decef2acc9b94957804a54969758f951
Joseph <joseph@appreciateco.com>
2019-04-08 05:46:05 -0700

    added an option autocomplete feature for Input

--- src/Input.js
+++ src/Input.js
@@ -1,39 +1,44 @@
-import React from 'react';
-  
+import React from 'react'
+
 class Input extends React.Component {
+	render() {
+		//make the password field type "password" so its contents are hidden
+		let type
+		if (this.props.name === 'password') {
+			type = 'password'
+		} else {
+			type = '"text"'
+		}
+		let lsr = 'lsr' + this.props.name
 
-  render() {
-    
-    //make the password field type "password" so its contents are hidden
-    let type;
-    if(this.props.name === 'password'){
-      type = 'password';
-    }else{
-      type = '"text"';
-    }
-    let lsr = 'lsr' + this.props.name;
+		let autoC
+		if (this.props.autoComplete === true) {
+			autoC = 'on'
+		} else {
+			autoC = 'off'
+		}
 
-      return (
-        <div className="rfa_input-container">
-        <p className="rfa_label">{this.props.label} </p>
-        <p className='rfa_input-error'>{this.props.error} </p>
-        <input  
-          type={type}
-          id={this.props.id} 
-          name={this.props.name} 
-          value={this.props.value}
-          onChange={this.props.onChange}
-          autoComplete="off"
-          className="rfa_textinput"
-        />
-        {this.props.lsr ? (
-          <div id={lsr} className="search-result">{this.props.lsr}</div>
-          ) : ( 
-          null
-          )}
-        </div>
-      );
-    }
-  }  
+		return (
+			<div className='rfa_input-container'>
+				<p className='rfa_label'>{this.props.label} </p>
+				<p className='rfa_input-error'>{this.props.error} </p>
+				<input
+					type={type}
+					id={this.props.id}
+					name={this.props.name}
+					value={this.props.value}
+					onChange={this.props.onChange}
+					autoComplete={autoC}
+					className='rfa_textinput'
+				/>
+				{this.props.lsr ? (
+					<div id={lsr} className='search-result'>
+						{this.props.lsr}
+					</div>
+				) : null}
+			</div>
+		)
+	}
+}
 
-export default Input;
+export default Input
