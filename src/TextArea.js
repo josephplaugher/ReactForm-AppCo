ece653266322973b63d5cf29e0335837c25c03a1
Joseph <joseph@appreciateco.com>
2019-04-15 16:22:12 -0700

    fixed the text area css classname

--- src/TextArea.js
+++ src/TextArea.js
@@ -1,33 +1,31 @@
-import React from 'react';
+import React from 'react'
 
 class TextArea extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		if (this.props.lsr) {
+			var lsr = 'lsr' + this.props.name
+		}
 
-    if(this.props.lsr) {
-      var lsr = 'lsr' + this.props.name;
-    }
-
-    return (
-      <div className="rfa_input-container">
-        <p className={this.props.labelClass}>{this.props.label} </p><br />
-         <textarea className="rfa_textarea"
-          id={this.props.id}
-          name={this.props.name}
-          onChange={this.props.onChange}
-          value={this.props.value}
-          rows={this.props.rows}
-          cols={this.props.cols}
-        />
-        <p className="rfa_textarea-errror-class">{this.props.error} </p>
-        { this.props.lsr ? (
-        <div id={lsr} className="rfa_search-result">{this.props.lsr}</div>
-        ) : ( null )}
-      </div>
-    );
-  }
+		return (
+			<div className='rfa_input-container'>
+				<p className='rfa_textarea-label'>{this.props.label} </p>
+				<br />
+				<textarea
+					className='rfa_textarea'
+					id={this.props.id}
+					name={this.props.name}
+					onChange={this.props.onChange}
+					value={this.props.value}
+					rows={this.props.rows}
+					cols={this.props.cols}
+				/>
+				<p className='rfa_input-error'>{this.props.error} </p>
+			</div>
+		)
+	}
 }
 
-export default TextArea;
+export default TextArea
